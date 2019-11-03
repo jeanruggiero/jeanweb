5c6e1ae9bc0f3e5eec187df431f0e15acf445e96
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-03 09:25:47 -0500

    Created views and prelim templates for home and resume pages. Reorganized content directories.

--- jean_site/views.py
+++ jean_site/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'jean_site/home.html')
+
+
+def resume(request):
+    return render(request, 'jean_site/resume.html')
