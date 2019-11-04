686950d2eeb2e73e1d97a0e0f01f0d37dce33b4e
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-04 15:42:37 -0500

    Static files collected, database prod settings modified

--- jeanweb/production.py
+++ jeanweb/production.py
@@ -1,17 +1,19 @@
 import os
 from .settings import *
 import django_heroku
+import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost',
                  'jeanweb.herokuapp.com']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jean_site/static')]
 
