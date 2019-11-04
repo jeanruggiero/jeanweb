4e2be6975576df30c8d2bf936a399b81828012d1
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-04 13:30:13 -0500

    Updated production settings

--- jeanweb/production.py
+++ jeanweb/production.py
@@ -3,6 +3,13 @@ from .settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost',
+                 'jeanweb.herokuapp.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jeanweb/static')]
