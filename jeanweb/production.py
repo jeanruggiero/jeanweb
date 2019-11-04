33539a23e009682145a65bb2fae19b74adcfa2ec
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-04 16:24:29 -0500

    Production settings update

--- jeanweb/production.py
+++ jeanweb/production.py
@@ -1,11 +1,11 @@
 import os
 from .settings import *
-import django_heroku
 import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost',
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'),
+                 'localhost',
                  'jeanweb.herokuapp.com']
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
@@ -16,6 +16,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jean_site/static')]
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
