2302ca19382fdb404effc14e1afb350291aa3456
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-09 16:23:25 -0500

    Created settings file for staging

---
+++ jeanweb/staging.py
@@ -0,0 +1,18 @@
+"""Staging app settings"""
+import os
+from .settings import *
+import dj_database_url
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'),
+                 'localhost',
+                 'jeanweb.herokuapp.com',
+                 'jeanweb-staging.herokuapp.com',]
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jean_site/static')]
