52e2c4e03e77856871a2fe6919581e5a478afc27
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-10 22:44:58 -0500

    Updated production settings to allow renamed heroku app host

--- jeanweb/production.py
+++ jeanweb/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'),
                  'localhost',
-                 'jeanweb.herokuapp.com']
+                 'jeanweb-prod.herokuapp.com']
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
