3203425d82c687aa520c8e3734dde48130893303
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-10 22:57:47 -0500

    Added custom domain to allowed hosts in production settings

--- jeanweb/production.py
+++ jeanweb/production.py
@@ -6,7 +6,9 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'),
                  'localhost',
-                 'jeanweb-prod.herokuapp.com']
+                 'jeanweb-prod.herokuapp.com',
+                 'jeanmruggiero.com',
+                 'www.jeanmruggiero.com']
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
