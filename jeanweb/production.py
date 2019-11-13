44fc8a54c4c62f62855c4393ff782dd53e5f260a
Jean Ruggiero <jeanruggiero@gmail.com>
2019-11-12 20:57:59 -0500

    Trying to fix issues with collectstatic.

--- jeanweb/production.py
+++ jeanweb/production.py
@@ -13,8 +13,4 @@ DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'jean_site/static')]
